a7ba038eecd9e5ab4daa78e5dafb81939dbf0d6a
Bliss Man <blissman@gmail.com>
2016-02-14 12:55:12 -0500

    add javascript to store clicked square values for x and o

--- ttt.js
+++ ttt.js
@@ -1,5 +1,7 @@
 $(document).on('ready', function() {
   var turn = 0;
+  var xSquares = [];
+  var oSquares = [];
 
   $('td').on('click', function() {
     // this === DOM Element
@@ -11,8 +13,14 @@ $(document).on('ready', function() {
 
     if ( turn % 2 ) {
       self.html('O').addClass('o');
+      //get value of square from id and push to oSquares array
+      oSquares.push(parseInt(self.attr('id').split('')[5]));
+      console.log(oSquares);
     } else {
       self.html('X').addClass('x');
+      //get value of square from id and push to xSquares array
+      xSquares.push(parseInt(self.attr('id').split('')[5]));
+      console.log(xSquares);
     }
 
     self.off('click');

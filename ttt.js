3820c5546b11c17a7090e505601eaac96ba341ff
Bliss Man <blissman@gmail.com>
2016-02-14 12:41:37 -0500

    modify html to magic square numbering

--- ttt.js
+++ ttt.js
@@ -1,6 +1,5 @@
 $(document).on('ready', function() {
   var turn = 0;
-  var winningCombos = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
 
   $('td').on('click', function() {
     // this === DOM Element
@@ -10,12 +9,6 @@ $(document).on('ready', function() {
     // Memoization = Store a computed output for repeated use
     var self = $(this);
 
-    // 0 % 2 === 0
-    // 1 % 2 === 1
-    // 2 % 2 === 0
-    // 3 % 2 === 1
-    // 4 % 2 === 0
-
     if ( turn % 2 ) {
       self.html('O').addClass('o');
     } else {
@@ -24,12 +17,14 @@ $(document).on('ready', function() {
 
     self.off('click');
 
-    checkForWinner();
-
     turn++;
+
+    if ( turn >= 4 ) {
+        checkForWinner();
+    }
   });
 
   function checkForWinner() {
-    // $('.x')
+    // Select all x's and sum. If any three elements === 15, then the user has won.
   }
 });

7f2e05f8da175b9f95558193fdffcd95704ec1cb
blissman <blissman@gmail.com>
2016-04-07 09:29:10 -0400

    add logic to prevent keypresses from overwriting previous moves

--- ttt.js
+++ ttt.js
@@ -9,48 +9,52 @@ $(document).on('ready', function() {
 
     // Idempotent = Always get the same outcome
     // Memoization = Store a computed output for repeated use
-    if (e.which === 113) {
+    if (e.which === 113 && $("#cell-2").hasClass("")) {
       var self = $("#cell-2");
     }
-    else if (e.which === 119) {
+    else if (e.which === 119 && $("#cell-7").hasClass("")) {
       var self = $("#cell-7");
     }
-    else if (e.which === 101) {
+    else if (e.which === 101 && $("#cell-6").hasClass("")) {
       var self = $("#cell-6");
     }
-    else if (e.which === 97) {
+    else if (e.which === 97 && $("#cell-9").hasClass("")) {
       var self = $("#cell-9");
     }
-    else if (e.which === 115) {
+    else if (e.which === 115 && $("#cell-5").hasClass("")) {
       var self = $("#cell-5");
     }
-    else if (e.which === 100) {
+    else if (e.which === 100 && $("#cell-1").hasClass("")) {
       var self = $("#cell-1");
     }
-    else if (e.which === 122) {
+    else if (e.which === 122 && $("#cell-4").hasClass("")) {
       var self = $("#cell-4");
     }
-    else if (e.which === 120) {
+    else if (e.which === 120 && $("#cell-3").hasClass("")) {
       var self = $("#cell-3");
     }
-    else if (e.which === 99) {
+    else if (e.which === 99 && $("#cell-8").hasClass("")) {
       var self = $("#cell-8");
     }
-
-    if ( turn % 2 ) {
-      self.html('<img src=cat.png height="100" width="100">').addClass('o');
-      //get value of square from id and push to oSquares array
-      oSquares.push(parseInt(self.attr('id').split('')[5]));
-    } else {
-      self.html('<img src=raccoon.png height="100" width="100">').addClass('x');
-      //get value of square from id and push to xSquares array
-      xSquares.push(parseInt(self.attr('id').split('')[5]));
+    else {
+      var self = "null"
     }
 
-    self.off('click');
-    turn++;
-    if ( turn >= 5 ) {
-      checkForWinner();
+    if ( self !== "null" ) {
+      if ( turn % 2 ) {
+        self.html('<img src=cat.png height="100" width="100">').addClass('o');
+        //get value of square from id and push to oSquares array
+        oSquares.push(parseInt(self.attr('id').split('')[5]));
+      } else {
+        self.html('<img src=raccoon.png height="100" width="100">').addClass('x');
+        //get value of square from id and push to xSquares array
+        xSquares.push(parseInt(self.attr('id').split('')[5]));
+      }
+
+      turn++;
+      if ( turn >= 5 ) {
+        checkForWinner();
+      }
     }
   });
 
